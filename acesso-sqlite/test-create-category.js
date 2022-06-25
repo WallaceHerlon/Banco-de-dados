06fa0e53e513a571519c2321d1390dc6ca9514be
Wallace Herlon <wherlon@hotmail.com>
2022-06-24 23:21:30 -0300

    CRUD categoria

---
+++ acesso-sqlite/test-create-category.js
@@ -0,0 +1,29 @@
+const sqlite = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+    const db = new sqlite.Database('banco.sqlite3', (err) => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve(db)
+        }
+    })
+})
+const run = (db, query, values) => new Promise((resolve, reject) => {
+    db.run(query, values, err => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve()
+        }
+    })
+})
+const createCategories = async () => {
+    const db = await initDB('banco.sqlite3')
+    await run(db, `INSERT INTO categories (id, category) VALUES (?, ?)`, [8, 'Nova cat'])
+
+    console.log('Categoriescreated!')
+}
+createCategories().catch(err => {
+    console.log(err)
+})
---
+++ acesso-sqlite/test-remove-category.js
@@ -0,0 +1,29 @@
+const sqlite = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+    const db = new sqlite.Database('banco.sqlite3', (err) => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve(db)
+        }
+    })
+})
+const run = (db, query, values) => new Promise((resolve, reject) => {
+    db.run(query, values, err => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve()
+        }
+    })
+})
+const removeCategories = async () => {
+    const db = await initDB('banco.sqlite3')
+    await run(db, `DELETE FROM categories WHERE id=?`, [8])
+
+    console.log('Categoriescreated!')
+}
+removeCategories().catch(err => {
+    console.log(err)
+})
