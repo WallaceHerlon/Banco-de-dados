cfab60b81b3df25fee6ffb642518e743873baadd
Wallace Herlon <wherlon@hotmail.com>
2022-07-01 20:15:04 -0300

    MySQL - Transações
    Reduzir o tempo gasto conectando-se ao servidor MySQL, reutilizando uma conexão anterior.

--- acesso-mysql/api.js
+++ acesso-mysql/api.js
@@ -1,19 +1,28 @@
 const db = require('./db')
 const categories = require('./categories')(db)
-const products = require( './products')(db)
+const products = require('./products')(db)
 
-const test = async() => {
+const test = async () => {
     //await categories.create(['New category from api'])
     //await categories.remove(6)
     //await categories.update(2, ['update from API'])
     //const cats = await categories.findAll()
     //console.log(cats)
-    
+
     //await products.addImage(3, ['img test', 'url'])
     //const prods = await products.findAllByCategory(1)
     //const prods = await products.findAllPaginated({ pageSize: 1, currentPage: 4})
     //console.log(prods)
-    await products.updateCategories(3, [6])
+    //await products.updateCategories(3, [6])
+
+    for (let i = 0; i < 1000; i++) {
+        products.findAllPaginated().then(prods => console.log(prods))
+    }
+
+
+
+
+
 
 }
 test()
--- acesso-mysql/db.js
+++ acesso-mysql/db.js
@@ -1,8 +1,20 @@
 const mysql = require('mysql2/promise')
 
-module.exports = mysql.createConnection({
+// without connection pool
+/*module.exports = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     port: 3312,
     database: 'cat-products'
+})*/
+
+//with connection pool
+module.exports = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3312,
+    database: 'cat-products',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
