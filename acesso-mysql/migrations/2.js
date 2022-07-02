86db17fbf8f6d8f9e622a742bc9691f6f8b93fa5
Wallace Herlon <wherlon@hotmail.com>
2022-07-01 21:08:09 -0300

    Migrations - Evoluindo o banco (up/down)

---
+++ acesso-mysql/migrations/2.js
@@ -0,0 +1,11 @@
+const up = async(Connection) => {
+    console.log('version 2 - up')
+}
+
+const down = async(Connection) => {
+    console.log('version 2 - down')
+}
+
+module.exports = {
+    up, down
+}
